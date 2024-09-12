41b436f15e73bdccf86f1a91f06579280684edda
antoineTauvelENSEA <84904367+antoineTauvelENSEA@users.noreply.github.com>
2024-09-12 14:45:32 +0200

    Pokemon party bug fixed

--- PokemonParty.cpp
+++ PokemonParty.cpp
@@ -2,14 +2,15 @@
 // Created by antotauv on 12/09/2024.
 //
 
+#include <algorithm>
 #include "PokemonParty.h"
 
 PokemonParty::PokemonParty(Pokeball * ppokeball, vector<int> vectorOfIndexInPokeball){
-    if (vectorOfIndexInPokeball.size()<=6){
-        for(int i =0; i<6;i++){
+    std::sort(vectorOfIndexInPokeball.begin(),vectorOfIndexInPokeball.end());
+
+    for(int i =0; i<6 && i<vectorOfIndexInPokeball.size();i++){
             this->arrayOfPokemon.push_back(ppokeball->getOnePokemonByIndexInList(vectorOfIndexInPokeball.at(i)-i));
         }
-    }
 }
 
 Pokemon * PokemonParty::getOnePokemonById(int id){
